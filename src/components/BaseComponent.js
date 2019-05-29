f1011b210379dfb0f28d988f1943bfe8c4c68fef
Winning12 <wenhao_jun@outlook.com>
2019-05-29 16:29:00 +0800

    addBaseComponent

---
+++ src/components/BaseComponent.js
@@ -0,0 +1,86 @@
+import { Component } from 'react';
+import {notification} from 'antd';
+
+var moment = require('moment');
+
+export class BaseComponent extends Component {
+    
+    // local
+    ip = "http://localhost:8080";  
+
+
+    post = (url, form, successAction) => {
+        return fetch(this.ip + url, { method: 'POST', body: form, header: { 'content-type': 'application/json' } })
+            .then((response) => (response.json()))
+            .catch((error) => { console.error(error); })
+            .then((result) => { handleResult(result,successAction); });
+    }
+
+    get = (url, successAction)=>{
+        return fetch(this.ip + url, { method: 'GET'})
+        .then((response) => (response.json()))
+        .catch((error) => { console.error(error); })
+        .then((result) => { handleResult(result,successAction); });
+    }
+
+    handleResult=(result,successAction) => {
+        if (!result) {
+            console.log(result)
+            this.pushNotification("danger", "连接错误");
+            return;
+        }
+
+        if (result.status === 'fail') {
+            console.log(result)
+            this.pushNotification("danger", result.description);
+            return;
+        }
+
+        if (result.status === 'success') {
+            successAction(result);
+            return;
+        } 
+        this.pushNotification("danger", result.description);
+    }
+
+    fromNow = (date) => {
+        return moment(date).fromNow()
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    hasErrors(fieldsError) {
+        return Object.keys(fieldsError).some(field => fieldsError[field]);
+    }
+
+    pushNotification = ( kind, reason ) => {
+        notification.config({
+            placement: 'topRight',
+            top: 80,
+            duration: 4,
+        });
+        if(kind=='danger')
+            notification.warning({
+                message:reason,
+                description:"糟了!",
+            })
+        else if(kind=='success')
+            notification.success({
+                message:reason,
+                description:"成功"
+            })
+        else
+            notification.open({
+                message:reason,
+                description:""
+            })
+    }
+    
+
+}
+
+export default BaseComponent;
