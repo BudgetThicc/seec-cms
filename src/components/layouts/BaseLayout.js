99bd3cf1c8ecb32dece078c6bba181696d531435
Winning12 <wenhao_jun@outlook.com>
2019-06-18 10:55:02 +0800

    fixBug

--- src/components/layouts/BaseLayout.js
+++ src/components/layouts/BaseLayout.js
@@ -42,7 +42,7 @@ class BaseLayout extends BaseComponent {
 
     getDefaultRoute=()=>{
         if(this.props.history.location.pathname.indexOf("/user")==-1)
-            if(!this.state.admin&&!this.props.admin){
+            if(!this.state.admin&&!this.props.sales){
                 return(<Redirect to={"/user/home"}/>)
             }
         return null
